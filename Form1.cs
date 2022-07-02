79be4647300699bbc5112c4d92e2be4f2a2d3d19
17860 <17860@DESKTOP-P8MUURT>
2022-07-02 13:22:21 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("是否查看窗体", "", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (dialogResult == DialogResult.Yes)
+            {
+            }
+            int deskWidth = Screen.PrimaryScreen.WorkingArea.Width;
+            int deskHeight = Screen.PrimaryScreen.WorkingArea.Height;
+            int width = Convert.ToInt32(deskWidth * 0.8);
+            int height = Convert.ToInt32(deskHeight * 0.8);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("是否关闭窗体", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dialogResult == DialogResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        
+    }
+}
